077486c793a156151313ac62c93418999fd1e130
Kolb <kolbi@hhmi.org>
2020-10-16 18:34:50 -0400

    plotly AP traces plot controls

--- python-plots/interactive_AP_plots.py
+++ python-plots/interactive_AP_plots.py
@@ -9,10 +9,33 @@ import numpy as np
 from scipy.io import loadmat
 import plotly.graph_objects as go
 import plotly.io as pio
+from plotly.subplots import make_subplots
+import plotly.express as px
 
 pio.templates.default = "plotly_white"
 
-def add_shaded_trace(t, y_mean, y_sterr):
+def add_shaded_trace(t, y_mean, y_sterr, stim_subplot_idx, color):
+    '''
+    plot AP mean trace +/- s.e.m
+
+    Parameters
+    ----------
+    t : 1d numpy array
+        time array (s).
+    y_mean : TYPE
+        DESCRIPTION.
+    y_sterr : TYPE
+        DESCRIPTION.
+    stim_subplot_idx : TYPE
+        DESCRIPTION.
+    color : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
     y_upper = y_mean + y_sterr
     y_lower = y_mean- y_sterr
     y_lower = np.flip(y_lower)
@@ -23,18 +46,21 @@ def add_shaded_trace(t, y_mean, y_sterr):
     x=np.append(t, t_rev),
     y=np.append(y_upper, y_lower),
     fill='toself',
-    fillcolor='rgba(0,100,80,0.2)',
+    fillcolor= color, # 'rgba(0,100,80,0.2)',
     line_color='rgba(255,255,255,0)',
     showlegend=False,
     name='GCaMP6s',
-    ))
+    opacity= 0.5
+    ), row=1,
+    col=stim_subplot_idx)
     
     # add line
     fig.add_trace(go.Scatter(
     x=t, y=y_mean,
-    line_color='rgb(0,100,80)',
-    name='GCaMP6s',
-    ))
+    line_color=color, # 'rgb(0,100,80)',
+    name='GCaMP6s'
+    ), row=1,
+    col=stim_subplot_idx,)
 
 plot_mat = loadmat(r'..\plotting.mat')
 
@@ -46,9 +72,23 @@ control = plot_mat['plot_out'][0,0]['control'] # control name string
 hits_med_med_dff_sterr = np.random.random_sample(hits_med_med_dff.shape)/10
 control_med_med_dff_sterr = np.random.random_sample(control_med_med_dff.shape)/10
 
-fig = go.Figure()
+n_stims = control_med_med_dff.shape[1]
+n_hits = hits_med_med_dff.shape[2]
 
-add_shaded_trace(time, control_med_med_dff[:,0], control_med_med_dff_sterr[:,0])
+stim_names = {'1', '3', '10', '160'}
+colorscheme = px.colors.qualitative.Plotly # 0th: control
+
+fig = make_subplots(rows=1, cols=n_stims) # go.Figure()
+
+# cycle through stim number
+for i in range(n_stims):
+    
+    # plot control
+    add_shaded_trace(time, control_med_med_dff[:,i], control_med_med_dff_sterr[:,i], i+1, colorscheme[0])
+    
+    # plot hits
+    for j in range(n_hits):
+        pass
 
 fig.update_traces(mode='lines')
 fig.update_layout(hovermode="closest", width=1400, height=800)
