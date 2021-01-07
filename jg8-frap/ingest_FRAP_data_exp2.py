jg8-frap: fix normalization

jg8-frap/ingest_FRAP_data_exp2.py
@@ -61,16 +61,16 @@ def decode_filename(fname):
 plt.close('all')
 
 save_figs       = False
-save_data       = True
+save_data       = False
 normalize_roi   = True
-keep_figs_open  = False # True to keep all generated figures open. Memory errors if too many open
+keep_figs_open  = True # True to keep all generated figures open. Memory errors if too many open
 
-bleachlaser_condition = 'stim405' # 'stim405' or 'stim405'
-solution_condition = 'iono' # 'regular' or 'iono'
-all_constructs = ['604.2', '10.641', '500.688','500.686'] #, '604.2','500.688','500.686'] # '500.688', '604.2', '500.686'
+bleachlaser_condition = 'stim488' # 'stim405' or 'stim488'
+solution_condition = 'regular' # 'regular' or 'iono'
+all_constructs =  ['604.2']# ['604.2', '10.641', '500.688','500.686']# ['604.2', '10.641', '500.688','500.686'] #, '604.2','500.688','500.686'] # '500.688', '604.2', '500.686'
 # all_cell_num = ['001.iono']# ['001', '002', '003', '004', '005']
 
-num_peaks_to_plot = 10 # 40 # 'all' to plot all
+num_peaks_to_plot = 1 # or 10 peaks for FULL
 length_to_plot = 200
 samples_pre_stim = 10 if bleachlaser_condition == 'stim405' else 20
 peak_thresh = 20000
@@ -121,8 +121,9 @@ for csv_filename in csv_filenames:
     
     print('total number of stims: {}'.format(len(idx_peaks)))
     if num_peaks_to_plot != 'all':
-        idx_peaks = idx_peaks[:num_peaks_to_plot]
-    
+        # idx_peaks_additional needed for plateau calculation
+        idx_peaks, idx_peaks_additional = (idx_peaks[:num_peaks_to_plot], idx_peaks[:num_peaks_to_plot+1])
+            
     # adjust peak indices to find rightmost peak
     for i,_ in enumerate(idx_peaks):
         idx_peaks[i] += np.where(roi1[idx_peaks[i]:idx_peaks[i] +20] > peak_thresh)[0][-1]
@@ -140,9 +141,9 @@ for csv_filename in csv_filenames:
     plateau_t = lambda idx: t[idx-plateau_start_idx:idx-plateau_end_idx]
     
     # change in plateaus over time
-    plateaus_roi1 = [plateau(i, roi1).mean() for i in idx_peaks] # plateaus from roi 2 (FRAP zone)
-    plateaus_roi2 = [plateau(i, roi2).mean() for i in idx_peaks] # plateaus from roi 2 (FRAP zone)
-    plateaus_roi3 = [plateau(i, roi3).mean() for i in idx_peaks] # plateaus from roi 3 (non-FRAP zone in same cell)
+    plateaus_roi1 = [plateau(i, roi1).mean() for i in idx_peaks_additional] # plateaus from roi 2 (FRAP zone)
+    plateaus_roi2 = [plateau(i, roi2).mean() for i in idx_peaks_additional] # plateaus from roi 2 (FRAP zone)
+    plateaus_roi3 = [plateau(i, roi3).mean() for i in idx_peaks_additional] # plateaus from roi 3 (non-FRAP zone in same cell)
     # plateaus_roi4 = [plateau(i, roi4).mean() for i in idx_peaks] # plateaus from roi 2 (FRAP zone)
     
     # plot stim-triggered averages
@@ -191,7 +192,7 @@ for csv_filename in csv_filenames:
             
     # percent change in plateaus (next plateau - current plateau ) / current plateau
     percent_change = np.diff(plateaus_roi2) / plateaus_roi2[0:-1]
-    
+    print(percent_change)
     if construct not in plateau_data:
         plateau_data[construct] = []
     plateau_data[construct].append(percent_change)
@@ -229,11 +230,11 @@ for csv_filename in csv_filenames:
 
 if save_data:
     
-    plateau_name = 'plateau_data_{}_{}_{}'.format('norm' if normalize_roi else '', solution_condition, stim_laser)
+    plateau_name = 'plateau_data_{}_{}_{}_{}peak'.format('norm' if normalize_roi else '', solution_condition, stim_laser, num_peaks_to_plot)
     with open(r'./analysis/'+ plateau_name + '.pkl', 'wb') as f:
         pickle.dump(plateau_data, f, pickle.HIGHEST_PROTOCOL)
     
-    roi_traces_name = 'roi_traces_{}_{}_{}'.format('norm' if normalize_roi else '', solution_condition, stim_laser)
+    roi_traces_name = 'roi_traces_{}_{}_{}_{}peak'.format('norm' if normalize_roi else '', solution_condition, stim_laser, num_peaks_to_plot)
     with open(r'./analysis/'+ roi_traces_name + '.pkl', 'wb') as f:
         pickle.dump(all_roi_avg_data, f, pickle.HIGHEST_PROTOCOL)
     

jg8-frap/plot_averages_2.py
@@ -14,7 +14,13 @@ def load_pkl(name):
 
 def get_mean_std(trace):
     return (np.mean(trace, axis=0), np.std(trace, axis=0))
-    
+
+
+def rescale(_trace, _max):
+    # _trace = _trace+1
+    _t = _trace * 1/(_max-np.min(_trace))
+    return _t - _t[0]# - np.min(_t)
+
 def get_trace_to_plot(trace_array, name):
     '''
 
@@ -34,30 +40,36 @@ def get_trace_to_plot(trace_array, name):
     # find min point of trace array
     (traces_mean, traces_std) = get_mean_std(trace_array)
     min_point_idx = traces_mean.argmin()
-    
+    print(min_point_idx)
     trace_array_cutoff = trace_array[:,min_point_idx:]
     trace_array_norm = np.zeros_like(trace_array_cutoff)
     for i,t in enumerate(trace_array_cutoff):
-        t = t-t[0]
-        t_fin = np.mean(t[-5:])
-        
+        # t = t-t[0]
+        prev_mean = 0# np.mean(t[:2])
         # stretch trace to fit between 0 and 1
-        trace_array_norm[i,:] = np.interp(t, (0, t_fin), (0, 1))
+        trace_array_norm[i,:] = rescale(t, prev_mean)# np.interp(t, (0, prev_mean), (0, 1))
+        # set baseline to 0 only
+        # trace_array_norm[i,:] = t
     
     # plot from lowest point to end
     (traces_norm_mean, traces_norm_std) = get_mean_std(trace_array_norm)
     t = np.arange(len(traces_norm_mean))/s_rate
     
     pd.DataFrame(trace_array_norm.T).to_csv(r'./analysis/normalized-csvs/' + name + '.csv')
+    # print(trace_array_norm[:,0])
     return(traces_norm_mean, traces_norm_std, t)
     
+one_peak = True # load single-peak only (for FRAP curve)
+
+peak_str = '_1peak' if one_peak else ''
+
 # load 405-bleached data
-traces_405 = load_pkl(r'./analysis/roi_traces_norm_regular_405.pkl')
-percents_405 = load_pkl(r'./analysis/plateau_data_norm_regular_405.pkl')
+traces_405 = load_pkl(r'./analysis/roi_traces_norm_regular_405' + peak_str + '.pkl')
+percents_405 = load_pkl(r'./analysis/plateau_data_norm_regular_405' + peak_str + '.pkl')
 
 # load 488-bleached data
-traces_488 = load_pkl(r'./analysis/roi_traces_norm_regular_488.pkl')
-percents_488 = load_pkl(r'./analysis/plateau_data_norm_regular_488.pkl')
+traces_488 = load_pkl(r'./analysis/roi_traces_norm_regular_488' + peak_str + '.pkl')
+percents_488 = load_pkl(r'./analysis/plateau_data_norm_regular_488' + peak_str + '.pkl')
 
 s_rate = 50
 
@@ -97,9 +109,9 @@ axs[1].errorbar(x-n_constructs/1000+(i+1)/500, 100*percent_mean, 100*percent_std
 
 construct_legend.append('604.2 (488 bleach)')
 
-# 0 lines
-axs[0].plot(t, np.zeros_like(t), 'k--')
-axs[1].plot(x, np.zeros_like(x), 'k--')
+# 1 lines
+axs[0].plot(t, np.ones_like(t), 'k--')
+axs[1].plot(x, np.ones_like(x), 'k--')
 
 
 axs[0].set_xlabel('time (s)')