jg8-frap: pretty plotting, fixing normalization again

jg8-frap/ingest_FRAP_data_all.py
@@ -49,7 +49,7 @@ import os, pickle
 def rescale(_trace, _max):
     # rescale so that _max value (plateau) = 1 and min value of trace = 0
     _t = _trace * 1/(_max-np.min(_trace))
-    return _t - _t[0] + 1
+    return _t - np.min(_t)
 
 def decode_filename(fname):
     '''
@@ -86,10 +86,10 @@ normalize_roi   = True
 keep_figs_open  = False # True to keep all generated figures open. Memory errors if too many open
 
 bleachlaser_condition = 'stim488' # 'stim405' or 'stim488'
-solution_condition = 'regular' # 'regular' or 'iono'
+solution_condition = 'iono' # 'regular' or 'iono'
 
 # ['604.2', '10.641'] # ['604.2', '10.641', '500.688','500.686']
-all_constructs =  ['604.2', '10.641']
+all_constructs = ['604.2', '10.641', '500.688','500.686']
 
 num_peaks_to_plot = 1 # or 10 peaks for FULL
 length_to_plot = 400
@@ -185,7 +185,8 @@ for csv_filename in csv_filenames:
             if i == 0:
                 current_roi = current_roi - plateaus_roi1[j]
             elif i == 1:
-                current_roi = rescale(current_roi, np.mean(current_roi[:5]))
+                # current_roi = rescale(current_roi, np.mean(current_roi[:5]))
+                current_roi = rescale(current_roi, plateaus_roi2[j])
             elif i == 2:
                 current_roi = current_roi - plateaus_roi3[j]
 #                elif i == 3:

jg8-frap/plot_averages_2.py
@@ -89,7 +89,7 @@ plt.close('all')
 colors = ['gray', 'red', 'blue', 'cyan', 'green']
 construct_legend = []
 percent_fig, axs = plt.subplots(2,1)
-
+percent_fig.set_size_inches([5.62, 6.85])
 # remove 604.2 (405 stim) from traces_405
 traces_405.pop('604.2')
 
@@ -132,31 +132,32 @@ axs[0].set_xlabel('Time (s)')
 plt.tight_layout()
 percent_fig.savefig(os.path.join('./analysis/normalized', 'percent_change.pdf'))
 
-'''
-axs[0].set_xlabel('time (s)')
-axs[0].set_ylabel('F (norm.)')
-axs[1].set_xlabel('stim number')
-axs[1].set_ylabel('Percent change (norm.)')
 axs[0].legend(construct_legend)
 
 
 ### 405 vs 488 comparison of 10.641
-_, axs = plt.subplots(2,1)
+f, axs = plt.subplots(2,1)
+f.set_size_inches([4.99, 6.56])
 
 (mean, std, t) = get_trace_to_plot(np.array(traces_405['10.641']), '10.641')
 (percent_mean_405, percent_std_405) = get_mean_std(np.array(percents_405['10.641']))
 axs[0].plot(t, mean, color = 'darkviolet')
 axs[0].fill_between(t, mean + std, mean - std, facecolor='darkviolet', color='darkviolet', alpha=0.2)
-axs[1].errorbar(x-n_constructs/1000+(i+1)/500, 100*percent_mean_405, 100*percent_std_405, color='darkviolet', capsize=5, marker='o')
+axs[1].bar(0, 100+100*percent_mean_405, yerr=100*percent_std_405, color='darkviolet')
+construct_legend = ['10.641 (405 bleach) (n={})'.format(len(percent_std_405))]
 
 (mean, std, t) = get_trace_to_plot(np.array(traces_488['10.641']), '10.641_488stim')
 (percent_mean_488, percent_std_488) = get_mean_std(np.array(percents_488['10.641']))
 axs[0].plot(t, mean, color = 'cyan')
 axs[0].fill_between(t, mean + std, mean - std, facecolor='cyan', color='cyan', alpha=0.2)
-# axs[0].set_ylim([-0.4, 0.4])
-
-
-axs[1].errorbar(x-n_constructs/1000+(i+1)/500, 100*percent_mean_488, 100*percent_std_488, color='cyan', capsize=5, marker='o')
-axs[0].legend(['10.641 (405 bleach)', '10.641 (488 bleach)'])
-'''
+axs[1].bar(1, 100+100*percent_mean_488, yerr=100*percent_std_488, color='cyan')
+axs[1].set_xticks([0,1])
+construct_legend = ['10.641 (405 bleach) (n={})'.format(len(traces_405['10.641'])), '10.641 (488 bleach) (n={})'.format(len(traces_488['10.641']))]
+axs[0].legend(construct_legend)
+axs[1].set_xticklabels(construct_legend)
+axs[0].set_ylabel('Recovery (%)')
+axs[0].set_xlabel('Time (s)')
+axs[1].set_ylabel('Recovery (%)')
+plt.tight_layout()
+f.savefig(os.path.join('./analysis/normalized', '405_vs_488_6s.pdf'))
 plt.show()

jg8-frap/regular_vs_iono_treatments.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
-
+import pickle, os
+from scipy import stats
 
 # plot average FRAP traces of all sensors
 # takes in roi_trace dict pkls from ingest_FRAP_data_exp2.py
@@ -60,41 +60,57 @@ for construct in all_constructs:
     
     if construct == '604.2': # if fungal GCaMP, use 488 data
         traces_regular_construct = traces_regular_488[construct]
+        percents_regular_construct = percents_regular_488[construct]
+        
     else:
         traces_regular_construct = traces_regular[construct]
+        percents_regular_construct = percents_regular[construct]
+
     
     traces_iono_construct = traces_iono[construct]
+    percents_iono_construct = percents_iono[construct]
     
     colors = ['gray', 'red']
+    construct_legend = ['regular (488 bleach)' if construct == '604.2' else 'regular', 'iono']
     
-    plt.figure()
-    plt.subplot(2,1,1)
+    construct_legend[0] += (' (n={})'.format(len(percents_regular_construct)))
+    construct_legend[1] += (' (n={})'.format(len(percents_iono_construct)))
+    
+    f = plt.figure(figsize = [4.99, 6.56])
+    f.suptitle(construct)
+    ax1 = plt.subplot(2,1,1)
     t = np.arange(traces_regular_construct[0].shape[0])/s_rate
-    plt.legend(['regular', 'iono'])
     (reg_mean, reg_std, t) = get_trace_to_plot(np.array(traces_regular_construct))
-    plt.plot(t, reg_mean, colors[0])
-    plt.fill_between(t, reg_mean + reg_std, reg_mean - reg_std, facecolor=colors[0], color=colors[0], alpha=0.2)
+    ax1.plot(t, reg_mean, colors[0])
+    ax1.fill_between(t, reg_mean + reg_std, reg_mean - reg_std, facecolor=colors[0], color=colors[0], alpha=0.2)
     
     (iono_mean, iono_std, t) = get_trace_to_plot(np.array(traces_iono_construct))
-    plt.plot(t, iono_mean, colors[1])
-    plt.fill_between(t, iono_mean + iono_std, iono_mean - iono_std, facecolor=colors[1], color=colors[1], alpha=0.2)
+    ax1.plot(t, iono_mean, colors[1])
+    ax1.fill_between(t, iono_mean + iono_std, iono_mean - iono_std, facecolor=colors[1], color=colors[1], alpha=0.2)
     
-    plt.plot(t, np.ones_like(t), 'k--')
-    plt.legend(['regular (488 bleach)' if construct == '604.2' else 'regular', 'iono'])
-    plt.show()
+    ax1.plot(t, np.ones_like(t), 'k--')
+    ax1.legend(construct_legend)
     # plt.ylim([-0.4, 0.4])
-    plt.title(construct)
-    plt.xlabel('Time (s)')
+    ax1.set_xlabel('Time (s)')
+    
     
     # percent change plot
-    percents_regular_construct = percents_regular[construct]
-    percents_iono_construct = percents_iono[construct]
-    plt.subplot(2,1,2)
+    
+    ax2 = plt.subplot(2,1,2)
     x = np.arange(1,percents_regular_construct[0].shape[0]+1)
-    (percent_reg_mean, percent_reg_std) = get_mean_std(np.array(percents_regular_construct))
-    (percent_iono_mean, percent_iono_std) = get_mean_std(np.array(percents_iono_construct))
-    plt.errorbar(x, 100*percent_reg_mean, 100*percent_reg_std, color=colors[0])
-    plt.errorbar(x, 100*percent_iono_mean, 100*percent_iono_std, color=colors[1])
-    plt.legend(['regular', 'iono'])
-    plt.xlabel('stim number')
-    plt.ylabel('Percent change (norm.)')
+    percents_regular_single_construct  = np.array(percents_regular_construct)
+    percents_iono_single_construct = np.array(percents_iono_construct)
+    (_,pval) = stats.ttest_ind(percents_regular_single_construct, percents_iono_single_construct, equal_var = False)
+    print(construct + ': regular vs iono: p = ' + str(pval))
+    (percent_reg_mean, percent_reg_std) = get_mean_std(percents_regular_single_construct)
+    (percent_iono_mean, percent_iono_std) = get_mean_std(percents_iono_single_construct)
+    ax2.bar(0, 100+100*percent_reg_mean, yerr = 100*percent_reg_std, color='black')
+    ax2.bar(1, 100+100*percent_iono_mean, yerr = 100*percent_iono_std, color='black')
+    # plt.errorbar(x, 100*percent_reg_mean, 100*percent_reg_std, color=colors[0])
+    # plt.errorbar(x, 100*percent_iono_mean, 100*percent_iono_std, color=colors[1])
+    ax2.set_xticks([0,1])
+    ax2.set_xticklabels(construct_legend)
+    ax2.set_ylabel('Recovery (%)')
+    plt.tight_layout()
+    f.savefig(os.path.join('./analysis/normalized', 'iono_' + construct + '.pdf'))
+