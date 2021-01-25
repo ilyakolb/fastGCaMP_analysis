3a4575c7e40461fd402d632a1adbf395662f1c76
Kolb <kolbi@hhmi.org>
2021-01-25 12:56:49 -0800

    culture-photobleaching save, titles, n

--- culture-photobleaching/analyze-photobleaching.py
+++ culture-photobleaching/analyze-photobleaching.py
@@ -18,6 +18,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+save_fig_dir_pdf = r'D:/ufgcamp_paper_data/culture-screen-figs/photobleaching/'
 plt.close('all')
 T_s = 10 # sampling period (s)
 
@@ -57,6 +58,9 @@ def get_column_name(table, plate_id, construct_name, cell_or_bg_num):
     return col_name[0]
     
 data = pd.read_csv('photobleaching-DR.csv')
+mapping = {'GCaMP6s': '10.641' , 'jGCaMP8f': '500.456', 'jGCaMP8m': '500.686', 'jGCaMP8s': '500.688','jGCaMP8.712': '500.712', 'GCaMP6f': '10.693', 'jGCaMP7f': '10.921', 'jGCaMP7s': '10.1473', 'jGCaMP7c': '10.1513', 'jGCaMP7b': '10.1561', 'XCaMP-Gf': '538.1', 'XCaMP-G': '538.2', 'XCaMP-Gf0': '538.3'} # [h[0] for h in hits[0]]
+mapping = {value:key for key, value in mapping.items()} # need to swap key-value for this
+
 hits = ['500dot456', '500dot686', '500dot688', '500dot712', '500dot543', '500dot707', '500dot640', '500dot455', '10dot921', '10dot1473', '10dot1513', '10dot1561', '538dot1']
 
 plate_names = ['P3a', 'P4a']
@@ -89,25 +93,30 @@ for h in hits:
 df_plt.loc['10dot1473']['bleach'].pop(0) # cell is all over the place
 df_plt.loc['10dot1513']['bleach'].pop(4) # cell fluorescence goes up
 
-plt.figure()
 
 i=1
 # plot mean +/- std for each sensor
 for index, row in df_plt.iterrows():
+    f = plt.figure(figsize=[3,3])
+    construct_name = mapping.get(index.replace('dot','.'), index)
     
-    ax = plt.subplot(4,4,i)
+    # ax = plt.subplot(4,4,i)
     i+=1
     bleach_nd = np.array(row['bleach']) # convert to 2d np array
     bleach_mean = np.mean(bleach_nd, axis = 0)
     bleach_std = np.std(bleach_nd, axis = 0)
     # _,ax= plt.subplots()
     plt.fill_between(t, bleach_mean+bleach_std, bleach_mean-bleach_std, 
-                     facecolor="blue", # The fill color
-                     color='blue',       # The outline color
+                     facecolor="black", # The fill color
+                     color='black',       # The outline color
                      alpha=0.2)          # Transparency of the fill)
-    ax.set_title(index)
+    plt.plot(t, bleach_mean, color='black')
+    plt.title(construct_name + ' (n=' + str(np.shape(bleach_nd)[0]) + ')')
     # ax.set_xlabel('Time (s)')
     # plt.tight_layout()
-    ax.set_ylim([0, 1.2])
-    
-plt.tight_layout()
+    plt.ylim([0, 1.2])
+    plt.xlabel('Time (s)')
+    plt.ylabel('F (norm.)')
+    plt.show()
+    plt.tight_layout()
+    f.savefig(save_fig_dir_pdf + 'photobleaching_' + construct_name + '.pdf')
